ac954e6d2c37c18afedc3b2017580aeec24f2d13
codycmartin <codycmartin89@gmail.com>
2019-11-14 14:54:05 -0800

    Labs Complete

---
+++ Module8/SimpleWinFormsApp/mainwindowform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWinFormsApp
+{
+    public partial class mainwindowform : Form
+    {
+        public mainwindowform()
+        {
+            InitializeComponent();
+        }
+
+        private void mainwindowform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+    }
+}
